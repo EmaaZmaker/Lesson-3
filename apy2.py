37b36365a0391aa702d551c1d8503bb9ca6f458c
EmaaZmaker <emaazbinammar@gmail.com>
2025-02-11 19:14:14 +0400

    String operations in python

---
+++ apy2.py
@@ -0,0 +1,8 @@
+message = "congratulations"
+
+uppercase_message = message.upper()
+
+print(uppercase_message)
+
+#mam this is Emaaz please note that some of the codes I copied because i din't understand clearly but some i wrote my self.
+#-EmaaZ
